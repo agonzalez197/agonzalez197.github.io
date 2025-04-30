a7e2c529bc066f0eab6f309bb7f202a50cfc7b29
agonzalez197 <agonzalez197@csustan.edu>
2025-04-30 09:17:40 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -33,9 +33,9 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: 'Presentation on Toxoplasma gondii ',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'This is a presentation discussing the parasite T.gondii including symptoms, epidemiology, and several case studies',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
           link: 'https://example.com',
